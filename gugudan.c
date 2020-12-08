c09467fa3072c71428b355192d65f1a67e4f81e3
dev-Wonjoon <70939090+dev-Wonjoon@users.noreply.github.com>
2020-12-09 02:03:04 +0900

    Update gugudan.c

--- gugudan.c
+++ gugudan.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main()
 {
-  int i,j;
-  for(i=2; i<10 i++)
-  {
-    for(j=1; i<10 j++)
+    int i, j;
+    for (i = 2; i < 10; i++)
     {
-      printf("%d * %d = %d",i,j,i*j)
+        for (j = 1; j < 10; j++)
+        {
+            printf("%d * %d = %d\n", i, j, i * j);
+        }
+        printf("\n");
     }
-  }
 }
